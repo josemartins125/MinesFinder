b95d7e18fa29f9d19802fd8f7f1f59cbedf2359e
José Martins <jpedrm@gmail.com>
2023-03-09 21:39:09 +0000

    commit das imagens

---
+++ src/main/java/MinesFinder.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class MinesFinder extends JFrame {
+    private JPanel painelPrincipal;
+
+    public MinesFinder(String title) {
+        super(title);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(painelPrincipal);
+        pack();
+    }
+    public static void main(String[] args) {
+        new MinesFinder("Mines Finder").setVisible(true);
+    }
+}
